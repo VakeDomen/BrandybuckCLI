a4e2f09063796d4becebc00c71a2e5f38d85c25b
Vake Domen <vake.domen94@gmail.com>
2020-08-20 19:32:26 +0200

    default id primary column for migration

--- src/helpers/migration_sqlite.rs
+++ src/helpers/migration_sqlite.rs
@@ -28,22 +28,19 @@ fn generate_tables_down(config_file: &ConfigFile, model_file: &ModelFile) -> Str
 fn generate_model_table(model: &Model) -> String {
     let mut table_rows = Vec::new();
     table_rows.push(String::from("CREATE TABLE ") + &model.name + " (");
+    table_rows.push(String::from("\tid VARCHAR PRIMARY KEY"));
     for col in model.fields.iter() {
         let mut column = String::from("\t") + &col.name + " " + &col.data_type + " ";
-        if col.key {
-            column += "PRIMARY KEY,";
+        if col.null {
+            column = column + "NULL,";
         } else {
-            if col.null {
-                column = column + " NULL,";
-            } else {
-                column = column + " NOT NULL,";
-            }
+            column = column + "NOT NULL,";
         }
         table_rows.push(column)
     }
     let row_len = &table_rows.len() - 1;
     let str_len = &table_rows[row_len.clone()].len() - 1;
-    table_rows[row_len].remove(str_len).clone();
+    table_rows[row_len].remove(str_len);
     table_rows.push(String::from(");\n"));
     table_rows.join("\n")
 }
